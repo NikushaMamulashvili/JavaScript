35ed3a20069dc9a728d7abae3a8a459d4335af5b
Nika Mamulashvili <164235570+NikushaMamulashvili@users.noreply.github.com>
2024-06-07 21:59:46 +0400

    Add files via upload

---
+++ JS2/script.js
@@ -0,0 +1,10 @@
+let myobject = {
+    firstname:"nika",
+    lastname:"mamulashvili",
+    address:"tbilisi, Asatiani",
+    age:16,
+    phonenumber:[
+        595523227
+    ]
+}
+console.log(myobject)
